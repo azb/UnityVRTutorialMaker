24f2885e8ca6271f1f358fde8967c0bd6452b500
Arthur Z. Baney <arthur.baney@leiainc.com>
2019-10-15 04:34:53 -0700

    Added floating help text window which points at a button, created necessary scripts to make it work

---
+++ UnityVRTutorialMaker/Assets/DrawLineBetweenObjects.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawLineBetweenObjects : MonoBehaviour
+{
+    public Transform object1, object2;
+
+    public Vector3 offset1, offset2;
+
+    LineRenderer line;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        line = GetComponent<LineRenderer>();
+        line.positionCount = 2;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        line.SetPosition(0, object1.position + offset1);
+        line.SetPosition(1, object2.position + offset2);
+    }
+}
---
+++ UnityVRTutorialMaker/Assets/RotateTowardsObject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateTowardsObject : MonoBehaviour
+{
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        float ydir = Mathf.Atan2( 
+            target.position.z - transform.position.z,
+            target.position.x - transform.position.x
+            ) * Mathf.Rad2Deg;
+
+        Debug.Log("ydir = "+ydir);
+
+        transform.rotation = Quaternion.Euler(0,-ydir,0);
+    }
+}
