ea7055e4fbcbd3ed31fc5aec2e7137880dff7094
Arthur Z. Baney <arthur.baney@leiainc.com>
2019-10-15 22:15:14 -0700

    Create UnplayKey.cs

---
+++ UnityVRTutorialMaker/Assets/UnplayKey.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnplayKey : MonoBehaviour
+{
+    public KeyCode key;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     if (Input.GetKeyDown(key))
+        {
+            Quit();
+        }
+    }
+
+    public static void Quit()
+     {
+         #if UNITY_EDITOR
+         UnityEditor.EditorApplication.isPlaying = false;
+         #elif UNITY_WEBPLAYER
+         Application.OpenURL(webplayerQuitURL);
+         #else
+         Application.Quit();
+         #endif
+     }
+}
