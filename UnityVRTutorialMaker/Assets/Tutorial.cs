1050168253fbe00ea5e50387290e4b7ea8f81331
Arthur Z. Baney <arthur.baney@leiainc.com>
2019-10-15 22:31:51 -0700

    Create Tutorial.cs

---
+++ UnityVRTutorialMaker/Assets/Tutorial.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour
+{
+
+    public enum Control {
+        RightControllerButton0,
+        RightControllerButton1,
+        RightControllerJoystick,
+        RightControllerTrigger,
+        RightControllerGrip,
+        LeftControllerButton0,
+        LeftControllerButton1,
+        LeftControllerJoystick,
+        LeftControllerTrigger,
+        LeftControllerGrip
+    };
+
+    public Transform 
+        RightControllerButton0,
+        RightControllerButton1,
+        RightControllerJoystick,
+        RightControllerTrigger,
+        RightControllerGrip,
+        LeftControllerButton0,
+        LeftControllerButton1,
+        LeftControllerJoystick,
+        LeftControllerTrigger,
+        LeftControllerGrip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
