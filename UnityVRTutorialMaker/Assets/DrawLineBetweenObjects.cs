03364623b434fb1286dcfa669d75b75d9ae57c6d
Arthur Z. Baney <arthur.baney@leiainc.com>
2019-10-17 02:23:02 -0700

    Update DrawLineBetweenObjects.cs

--- UnityVRTutorialMaker/Assets/DrawLineBetweenObjects.cs
+++ UnityVRTutorialMaker/Assets/DrawLineBetweenObjects.cs
@@ -15,13 +15,21 @@ public class DrawLineBetweenObjects : MonoBehaviour
     {
         line = GetComponent<LineRenderer>();
         line.positionCount = 2;
-
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (object1 != null)
         line.SetPosition(0, object1.position + offset1);
+        else
+            Debug.LogError("object1 is null");
+
+        if (object2 != null)
         line.SetPosition(1, object2.position + offset2);
+        else
+            Debug.LogError("object2 is null");
+
+
     }
 }
