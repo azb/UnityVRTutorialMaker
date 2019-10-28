e64abd1e1313533cf8364f5549f7a600d125e31b
Arthur Z. Baney <arthur.baney@leiainc.com>
2019-10-28 10:19:52 -0700

    Added Rotate.cs for rotating the tutorial block in the scene

---
+++ UnityVRTutorialMaker/Assets/VRTutorialMaker/Rotate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VRTutorializer
+{
+public class Rotate : MonoBehaviour {
+
+    public Vector3 rotationsPerSecond;
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(
+            rotationsPerSecond.x * 360 * Time.deltaTime, 
+            rotationsPerSecond.y * 360 * Time.deltaTime, 
+            rotationsPerSecond.z * 360 * Time.deltaTime
+            );
+	}
+}
+}
