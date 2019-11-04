33ec1b1bdf93ec48c0776297332865a0deb0e6bc
Arthur Z. Baney <arthur.baney@leiainc.com>
2019-11-03 21:19:45 -0800

    Create TutorialScriptableObjectSpawner.cs

---
+++ UnityVRTutorialMaker/Assets/VRTutorialMaker/Scripts/TutorialScriptableObjectSpawner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VRTutorializer
+{
+[DefaultExecutionOrder(-20)]
+public class TutorialScriptableObjectSpawner : MonoBehaviour
+{
+    public TutorialScriptableObject tutorialSO;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        tutorialSO.Initialize();
+    }
+}
+}
