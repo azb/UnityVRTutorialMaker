94fd9b7a1309c2c4cba5b99ad84eee3851ca729b
Arthur Z. Baney <arthur.baney@leiainc.com>
2020-02-22 03:24:22 -0800

    Update FloatTowards.cs
    Can now specify one or two targets for it to float towards.
    If there is two, it will float to the average position of the two targets.

--- UnityVRTutorialMaker/Assets/VRTutorialMaker/Scripts/FloatTowards.cs
+++ UnityVRTutorialMaker/Assets/VRTutorialMaker/Scripts/FloatTowards.cs
@@ -3,28 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace VRTutorializer
-{ 
-public class FloatTowards : MonoBehaviour
 {
-    public Transform target1, target2;
-
-    public Vector3 offset;
-
-    public float speed;
-
-    public float distance;
-    
-    // Update is called once per frame
-    void Update()
+    public class FloatTowards : MonoBehaviour
     {
-        if (target1 != null && target2 != null)
-        transform.position = (target1.position + target1.forward * distance + target2.position + target2.forward * distance) / 2f + offset;
+        public Transform target1, target2;
 
-        //transform.position = Vector3.Lerp( 
-        //    transform.position,
-        //    (target1.position + target1.forward * distance + target2.position + target2.forward * distance) / 2f + offset,
-        //    Time.deltaTime * speed
-        //    );
+        public Vector3 offset;
+
+        public float speed;
+
+        public float distance;
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (target1 != null && target2 != null)
+            {
+                transform.position = (target1.position + target1.forward * distance + target2.position + target2.forward * distance) / 2f + offset;
+            }
+            else
+            {
+                if (target1 != null)
+                {
+                    transform.position = target1.position;
+                }
+            }
+        }
     }
-}
 }
